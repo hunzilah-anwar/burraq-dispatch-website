bb6361e907291e542606acc5bbb5d216a7abd6b4
hunzilah-anwar <hunzilahanwar786@gmail.com>
2026-07-22 14:07:12 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ function App() {
     <BrowserRouter>
     <ScrollToTop />
       <Header />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
